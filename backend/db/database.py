588d6915954876ee258a4b33501306ccae57cc00
henriquecmelo1 <hcm@cin.ufpe.br>
2025-05-27 11:06:41 -0300

    fix: backend now waits for db to be ready

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,13 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.exc import OperationalError
 from dotenv import load_dotenv
-import os
+import os, time
 
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+# engine = create_engine(DATABASE_URL)
+
+for i in range(10):
+    try:
+        engine = create_engine(DATABASE_URL)
+        connection = engine.connect()
+        print("Banco conectado com sucesso!")
+        break
+    except OperationalError:
+        print(f"Tentando conectar... ({i+1}/10)")
+        time.sleep(2)
+else:
+    print("Não foi possível conectar ao banco.")
+    raise Exception("Database not ready")
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
