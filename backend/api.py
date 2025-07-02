14be500ea3e382fe64416ed0a9db7c8ed9b1e5a3
henriquecmelo1 <hcm@cin.ufpe.br>
2025-07-02 09:35:02 -0300

    fix: CORS

--- backend/api.py
+++ backend/api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React app
+    allow_origins=["http://localhost:5173", "https://drink-rats.vercel.app"],  # React app
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
