7c3ac64f7ca9a453ecf31462b6a8f1b52c8e53a1
henriquecmelo1 <hcm@cin.ufpe.br>
2025-07-02 09:02:48 -0300

    fix: database url

--- backend/db/database.py
+++ backend/db/database.py
@@ -10,7 +10,7 @@ POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
-engine = create_engine(f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DB}')
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
